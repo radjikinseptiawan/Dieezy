33a29dc468b10dc6f3de96f39be537569e1d410b
radjikinseptiawan <radjikins21@gmail.com>
2025-03-10 09:51:54 +0700

    Update Readme.md

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-Sebuah project fullstack sederhana berbasis sosial media yang bertujuan untuk melatih skill agar dapat membuat project yang sedikitlebih kompleks namun tidak sekompleks sosial media lain
+Sebuah project fullstack sederhana berbasis sosial media yang bertujuan untuk melatih skill agar dapat membuat project yang sedikit lebih kompleks namun tidak sekompleks sosial media lain
 
 
 ## Feature
 - Feature an authentication system
 - Social Post Feature
+
+## TechStack
+- NextJS
+- TailwindCSS
+- Bcrypt
+- axios
+- MongoDB
+- NextAuth
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 "use client"
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -21,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} antialiased`}
       >
         <SessionProvider>
           {children}
